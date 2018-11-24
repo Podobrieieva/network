2f4eeef9e01c198f4546a5502722960e2c20e327
Podobrieieva <juliapodobr@gmail.com>
2018-11-24 17:31:41 +0200

    add interface

--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -66,9 +66,9 @@ export interface UserProfileModel {
 }
 
 export interface CommentModel{
-    avatar?: string;
+    avatar: string;
     content:string;
-    userName?:string;
+    userName:string;
     
 }
 
@@ -78,6 +78,104 @@ export interface PostModel{
     userName:string;
     image?: string;
     id: number;
+}
+
+//////
+
+export interface UserData {
+    name: string;
+    surname: string;
+    email: string;
+    photo: string;
+    _token?: string;
+    age: number | string;
+    birthDate: Date;
+}
+
+export interface UserCard {
+    name: string;
+    surname: string;
+    photo: string;
+    id: number
+}
+
+export interface Post {
+    id: string;
+    photo: string;
+    header: string;
+    text: number | string;
+    comments: Array<PostComment>;
+}
+
+export interface PostComment {
+    id: string;
+    user: UserCard;
+    text: string;
+    childrensId: Array<string>;
+}
 
 
+export interface UserDataRequest {
+    email: string;
+    password: string;
+}
+
+export interface UserDataResponse {
+    user: UserData;
+    userPosts: Array<Post>;
+    subscibtionsCount: number | string;
+    subscribersCount: number | string;
+}
+
+export interface UserChangeDataRequest {
+    fieldName: string;
+    newValue: string;
+    token: string;
+}
+
+export interface UserChangeDataResponse {
+    done: boolean;
+    user: UserData;
+}
+
+export interface PostAddRequest {
+    newPost: Post;
+    token: string;
+}
+
+export interface PostAddResponse {
+    done: boolean;
+    userPosts: Array<Post>;
+}
+
+export interface CommentAddRequest{
+    idPost: string;
+    idComment: string | null;
+    newComment: PostComment;
+    token: string;
+}
+
+export interface CommentAddResponse{
+    done: boolean;
+    updatePost: Post;
+}
+
+export interface SubscribersRequest {
+    id: string;
+    token: string;
+}
+
+export interface SubscribersResponse {
+    done: boolean;
+    subscribers: Array<UserCard>;
+}
+
+export interface SubscribtionsRequest {
+    id: string;
+    token: string;
+}
+
+export interface SubscribtionsResponse {
+    done: boolean;
+    subscribtions: Array<UserCard>;
 }
