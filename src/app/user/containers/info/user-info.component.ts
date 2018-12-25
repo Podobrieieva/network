4da50353c7f97cbb61d53ea845976f3aa70821a7
Entrant <Trueentrant@gmail.com>
2018-12-25 20:28:00 +0200

    info component finished

--- src/app/user/containers/info/user-info.component.ts
+++ src/app/user/containers/info/user-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-info',
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-info.component.scss']
 })
 export class UserInfoComponent implements OnInit {
+  @Input() data: any;
+  @Input() owner: boolean;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public testFunction () {
+    console.log(this.data);
+  }
 }
